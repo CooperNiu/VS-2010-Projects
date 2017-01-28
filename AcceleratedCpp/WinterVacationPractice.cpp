49e0aab0ad576876b83bd5411078e373c3a6fa50
CooperNiu <niuyanno.1@outlook.com>
2017-01-28 16:38:53 +0800

    Practice2-4

--- AcceleratedCpp/WinterVacationPractice.cpp
+++ AcceleratedCpp/WinterVacationPractice.cpp
@@ -1,48 +1,81 @@
 #include<iostream>
 #include <string>
+#include <iomanip>
+#include <ios>
 
 using namespace std;
 
 int main()
 {
-	int a=1, b=1;
-	cout << "Hello, world!"<<endl;
-	cout<<a+b<<endl;
-	cout << "\t This \(\"\) is a quote, and this \(\\\) is a backlash."<<endl;
-	/* 
-	Hello, world!
-	2
-		This (") is a quote, and this (\) is a backlash.
-	 */
+    //ask for and read the student's name
+    cout<<"Please enter your midterm and final exam grades: ";
+    double midterm, final;
+    cin>>midterm>>final;
 
-	cout<<"Please input your first name:";
-	string name;
-	cin >> name;
+    //ask for the homework grades
+    cout<<"Enter all your homework grades,"
+        "followed by end-of-file: ";
 
-	// build the message that we intend to write
-	string greeting = "Hello, " + name + "!";
+    //the number and sum of grades read so far
+    int count = 0;
+    double sum = 0;
 
-	// build the second and fourth lines of the output
-	string spaces(greeting.size(), ' ');
-	string second = "* " + spaces + " *";
+    //a viriable into which to read
+    double x;
 
-	//build the first and fifth lines of the output
-	const string first(second.size(), '*');
+    //invariant
+    //we have read count grades so far, and
+    //sum is the sum of the first count grades
+    while(cin>>x)
+    {
+        ++count;
+        sum+=x;
+    }
 
-	//write it all
-	cout << endl;
-	cout << first << endl;
-	cout << second << endl;
-	cout << "* " << greeting << " *"<<endl;
-	cout << second << endl;
-	cout << first << endl;
-
-	// use some functions
-	char c = '@';
-	int n=10;
-	string z(n, c); // initial z with n copies of c, n is the number of characters, c is the character;
-	cout<< z << endl;
+    // write the result
+    streamsize prec = cout.precision();
+    cout << "Your final grade is " << setprecision(3)
+    <<0.2*midterm + 0.4 * final + 0.4 * sum / count
+    << setprecision(prec) << endl;
 
 	system("pause");
 	return 0;
 }
+
+//int a=1, b=1;
+//cout << "Hello, world!"<<endl;
+//cout<<a+b<<endl;
+//cout << "\t This \(\"\) is a quote, and this \(\\\) is a backlash."<<endl;
+///*
+//Hello, world!
+//2
+//    This (") is a quote, and this (\) is a backlash.
+// */
+//
+//cout<<"Please input your first name:";
+//string name;
+//cin >> name;
+//
+//// build the message that we intend to write
+//string greeting = "Hello, " + name + "!";
+//
+//// build the second and fourth lines of the output
+//string spaces(greeting.size(), ' ');
+//string second = "* " + spaces + " *";
+//
+////build the first and fifth lines of the output
+//const string first(second.size(), '*');
+//
+////write it all
+//cout << endl;
+//cout << first << endl;
+//cout << second << endl;
+//cout << "* " << greeting << " *"<<endl;
+//cout << second << endl;
+//cout << first << endl;
+//
+//// use some functions
+//char c = '@';
+//int n=10;
+//string z(n, c); // initial z with n copies of c, n is the number of characters, c is the character;
+//cout<< z << endl;
--- AcceleratedCppPractice2-4/AcceleratedCppPractice2-4/Practice2-4.cpp
+++ AcceleratedCppPractice2-4/AcceleratedCppPractice2-4/Practice2-4.cpp
@@ -2,47 +2,153 @@
 // 
 #include <iostream>  
 #include <string>  
+
 using namespace std;  
+
+void square(int side);
+void rectangle(int row5, int col5);
+void triangle(int side);
+
 int main()  
 {  
-	cout<<"input your name:";  
-	string name;  
-	cin>>name;  
+	int practice = 0;
+	for (;;) // 死循环，可以多次输入验证不同的题目结果
+	{
+		cout<<"Please choose Practice num:"<<endl;
+		cin>>practice;
+		switch (practice)
+		{
+			case 4:
+				{
+					cout<<"input your name:";  
+					string name;  
+					cin>>name;  
 
-	cout<<endl<<endl;  
-	const int pad=3;  
-	const string space(pad,' ');  
-	const string greet=space+"Hello, "+name+space;  
-	const string ps(greet.size(),' ');  
-	const int row=pad*2+3;  
-	const string::size_type col=greet.size()+2;  
+					cout<<endl<<endl;  
+					const int pad=3;  //空行数量
+					const string space(pad,' ');  
+					const string greet=space+"Hello, "+name+space;  
+					const string ps(greet.size(),' ');  
+					const int row=pad*2+3;  
+					const string::size_type col=greet.size()+2;  
 
-	for(int r=0;r!=row;++r)  
-	{  
-		string::size_type c=0;  
-		while(c!=col)  
-		{  
-			if(r==pad+1 && c==1)  
-			{  
-				cout<<greet;  
-				c+=greet.size();  
-			}  
-			else   
-			{  
-				if(r==0||r==row-1||c==0||c==col-1)  
-				{  
-					cout<<"*";  
-					++c;  
-				}  
-				else if(c==1)  
-				{  
-					cout<<ps;  
-					c+=ps.size();  
-				}  
-			}  
-		}  
-		cout<<endl;  
-	}  
-	system("pause");
+					for(int r=0;r!=row;++r)  
+					{  
+						string::size_type c=0;  
+						while(c!=col)  
+						{  
+							if(r==pad+1 && c==1)  
+							{  
+								cout<<greet;  
+								c+=greet.size();  
+							}  
+							else   
+							{  
+								if(r==0||r==row-1||c==0||c==col-1)  
+								{  
+									cout<<"*";  
+									++c;  
+								}  
+								else if(c==1)  
+								{  
+									cout<<ps;  
+									c+=ps.size();  
+								}  
+							}  
+						}  
+						cout<<endl;  
+					}  
+					break;
+				}
+
+			case 5:
+				{
+					int shape=0;
+					int length;
+					int row5, col5;
+					cout<<"choose 1. square, 2. rectangle, 3. triangle"<<endl;
+					cin>>shape;
+					switch (shape)
+					{
+					case 1:	
+						{	
+							cout<<"Please input the length of side";
+							cin>>length;
+							square(length);
+							break;
+						}
+					case 2:
+						{
+							cout<<"Please input rows and columns"<<endl;
+							cin>>row5>>col5;
+							rectangle(row5, col5);
+							break;
+						}
+					case 3:
+						{
+							cout<<"Please input the length of side";
+							cin>>length;
+							triangle(length);
+							break;
+						}//case
+					}//switch
+				}//case
+
+			case 7:
+				{
+					countdown();
+					break;
+				}
+		}//switch
+	}
 	return 0;  
 }  
+
+//output square
+void square(int side)
+{
+	for (int i=0; i<side; i++)
+	{
+		for (int j=0; j<side; j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+//output rectangle
+void rectangle(int row5, int col5)
+{
+	for (int i=0; i<row5; i++)
+	{
+		for (int j=0; j<col5; j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+// output triangle
+void triangle(int side)
+{
+	for (int i=0; i<side; i++)
+	{
+		for (int j=0; j<i; j++)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
+
+void countdown()
+{
+	for(int i=-5; i < 10; i++)
+	{
+		cout<<i;
+		i++;
+		cout<<endl;
+	}
+}
